58b592491ba212421a01c0964cb242e9fee6062e
Ryland18 <ryrex1808@icloud.com>
2025-04-16 20:34:19 -0500

    update

--- Wordsearch.py
+++ Wordsearch.py
@@ -1,32 +1,73 @@
+import csv
+
 def importWords():
      listOfWords = ["Binary", "Computer", "Hexadecimal", "Bander", "Powershell", "Apps", "Web Sites", "Duck", "VSC", "Poptarts"]
      return listOfWords
 
-importWords()
-board = ""
+words = importWords()
+board = []
 
 def importBoard():
-     with open('Practice Word Search.xlsx','r') as file:
-          board = file.read()
+     with open('PracticeWordSearch.csv',mode='r') as file:
+          boardset = csv.reader(file)
+          for i in boardset:
+               board.append(i)     
+          
      return board
 
 importBoard()
 
-print(board)
-
 def printBoard(board):
+     col_widths = [max(len(str(item)) for item in col) for col in zip(*board)]
+     for row in board:
+            print(" | ".join(f"{item:<{col_widths[i]}}" for i, item in enumerate(row)))
      pass
 
-#def searchHorizontally(word,board):
-     return
+printBoard(board)
 
-#def searchVerically(word,board):
+
+
+def searchHorizontally(listOfWords,board):
+     result = []
+     for i in range (len(board)):
+          horizon = ''.join(board[i])
+          for t in range(len(listOfWords)):
+               if listOfWords[t] in horizon:
+                    result.append(t)
+     return result
+
+
+print(searchHorizontally(words,board))
+
+
+def searchVerically(words,board):
+     
+     item = []
+     
+     for i in range(len(col_widths = [max(len(str(item)) for item in col) for col in zip(*board)])):
+          for t in range(len(board)):
+               item.append(board[i][t])
+     #item = []
+     #for i in range(len(board)):
+     #     item.append(board[i][1])
+     #column = ''.join(item)
+     #for i in words:
+     #     if i in column:
+     #          print(i)
      return
 
 #def searchRightDiagonal(word,board):
-     return
+#     item = []
+#     for i in range(len(board)):
+#          item.append(board[i][i])
+#     diagnal = ''.join(item)
+#     for i in word:
+#          if 
 
-#def searchLeftDiagonal(word,board):
+     return
+def searchLeftDiagonal(word,board):
+
+
      return
 
 #wordSearchBoard=importBoard()
